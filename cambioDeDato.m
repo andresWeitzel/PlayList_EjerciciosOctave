7c923164b2cd57bce0dffb64d5ed9ce530133a1b
andresWeitzel <andres96energy@hotmail.com>
2021-02-20 22:16:56 -0300

    Add files via upload

---
+++ cambioDeDato.m
@@ -0,0 +1,15 @@
+%{
+
+Realice 2 Operaciones Aritmeticas definidas sobre la variable
+'resultado' y el valor de la misma
+convertirla a un numero Entero de 2 bytes
+
+%}
+
+resultado=(12+45)*2;
+
+resultado=int16(resultado);
+
+whos()
+
+
