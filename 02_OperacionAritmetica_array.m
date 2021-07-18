2b8e33bc6e3e1ba7d77ca75beb6a5a14a8629f1d
andresWeitzel <andres96energy@hotmail.com>
2021-07-18 22:20:35 +0000

    Add files via upload

---
+++ 02_OperacionAritmetica_array.m
@@ -0,0 +1,22 @@
+%{
+Realizar 3 Operaciones Aritmeticas cualesquiera y cada una de ellas
+almacenarlas en 3 variables diferentes, luego mostrar los resultados 
+por consola y a su vez almacenar las variables en un Array de tipo 
+vector.
+%}
+
+operacionAritmetica01=132+2;
+operacionAritmetica02=456/7;
+operacionAritmetica03=3434*4;
+
+disp("\n")
+disp(operacionAritmetica01)
+disp(operacionAritmetica02)
+disp(operacionAritmetica03)
+disp("\n")
+
+vectorOperacionesAritmeticas=[
+  operacionAritmetica01
+  ,operacionAritmetica02
+  ,operacionAritmetica03
+  ]
